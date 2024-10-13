1385e3cf7ddcfe1305b31f1206cf2c78b5be7ced
SwissPowershell <Supreme@sefanet.ch>
2024-10-13 12:36:34 +0200

    Initial Commit

--- SPS-RoyalTS.psm1
+++ SPS-RoyalTS.psm1
@@ -1,3 +1,42 @@
+Add-Type -AssemblyName System.Windows.Forms
+Enum MessageType {
+    Information
+    Warning
+    Error
+}
+Function Write-RTSLog {
+    [CmdletBinding()]
+    Param(
+        [String] ${Message},
+        [MessageType] ${Type} = [MessageType]::Information
+    )
+    $LogPath = Join-Path -Path $Env:APPDATA -ChildPath 'SPS-RoyalTS\RoyalTSLog.log'
+    if (-not (Test-Path -Path $LogPath)) {
+        Try {
+            New-Item -Path $LogPath -ItemType File -Force | Out-Null
+        }Catch {
+            Throw "An unexpected error occured while creating the log file $($LogPath): $($_.Exception.Message)"
+        }
+        
+    }
+    $LogMessage = "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] [$($Type.ToString())] $($Message)"
+    Try {
+        Add-Content -Path $LogPath -Value $LogMessage | Out-Null
+    }Catch{
+        Throw "An unexpected error occured while writing the log message $($LogMessage) to the log file $($LogPath): $($_.Exception.Message)"
+    }
+}
+Function Show-RTSMessageBox {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true,Position=0)]
+        [String] ${Message},
+        [String] ${Title} = 'SPS RoyalTS Module',
+        [System.Windows.Forms.MessageBoxButtons] ${Buttons} = [System.Windows.Forms.MessageBoxButtons]::OK,
+        [System.Windows.Forms.MessageBoxIcon] ${Icon} = [System.Windows.Forms.MessageBoxIcon]::Information
+    )
+    [System.Windows.Forms.MessageBox]::Show($Message, $Title, $Buttons, $Icon)
+}
 Function New-DefaultRoyalTSConfiguration {
     [CmdletBinding()]
     Param (
@@ -7,135 +46,92 @@ Function New-DefaultRoyalTSConfiguration {
     )
     Write-Verbose -Message "Processing the function $($MyInvocation.MyCommand)"
     # Create the configuration file
-    $Configuration = @'
-<?xml version="1.0" encoding="utf-8"?>
-<RoyalTSConfiguration>
-<!-- Help for the RoyalTSConfiguration file
-    # Remote file
-    You can define a remote file instead of using this configuration file the remote will be used as a source
-
-    # Dynamic folder
-    You can define a set of rules to create dynamic folder or add single computers
-        ## Rules
-            ### ADGroupRules
-                The AD Group rule intend to add computers based on AD groups
-                You can add multiple AD groups
-                The fields are as follow :
-                    Name : The name of the AD group (mandatory,Accepts wildcards)
-                    Domain : The domain of the AD group (mandatory,Accepts wildcards,Accept Tokens)
-                    Regexp : a Regularexpression (Optional)
-                        Regex : the regular expression to apply (mandatory)
-                        RXGroupOrder : if the regular expression contains groups you can define the order of the groups to use as subfolders (Optional)
-            ### ComputerNameRules
-                The Computer Name rule intend to add computers based on their name
-                You can add multiple computer names
-                The fields are as follow :
-                    Name : The name of the computer (mandatory,Accepts wildcards,Accept Tokens)
-                    Domain : The domain of the computer (mandatory,Accepts wildcards,Accept Tokens)
-                    Regexp : a Regularexpression (Optional)
-                        Regex : the regular expression to apply (mandatory)
-                        RXGroupOrder : if the regular expression contains groups you can define the order of the groups to use as subfolders (Optional)
-        ## SingleComputers
-            You can add some single computers using their name
-            the fileds are as follow :
-                Name : The name of the computer (mandatory,Accepts wildcards,Accept Tokens)
-                Domain : The domain of the computer
-                UserName : The username to use to connect to the computer
+    $DefaultConfigPath = "$($PSScriptRoot)\DefaultConfig.xml"
+    if ((Test-Path -path $DefaultConfigPath -ErrorAction Ignore) -eq $True) {
+        Try {
+            [XML] $Configuration = Get-Content -Path $DefaultConfigPath -ErrorAction Stop
+        }Catch {
+            Throw $_
+        }
+        # Create the path if it does not exist
+        if ((Test-Path -Path $Path -ErrorAction Ignore) -eq $False) {New-Item -Path $Path -ItemType File -Force | Out-Null}
+        # Save the configuration in the default path
+        Try {
+            $Configuration.Save($Path)
+        }Catch{
+            Throw $_
+        }
         
-        ! the default file contain one example for each type of rule
-    
-    ! Token and automatic variable
-        the following token can be used in the configuration file
-        $Name$ : The name of the dynamic folder
-    
--->
-    <RemoteFile></RemoteFile>
-    <DynamicFolder>
-        <Rules>
-            <ADGroupRules>
-                <ADGroup>
-                    <Name></Name>
-                    <Domain></Domain>
-                    <Regexp>
-                        <Regex></Regex>
-                        <RXGroupOrder></RXGroupOrder>
-                    </Regexp>
-                </ADGroup>
-            </ADGroupRules>
-            <ComputerNameRules>
-                <ComputerName>
-                    <Name></Name>
-                    <Domain></Domain>
-                    <Regexp>
-                        <Regex></Regex>
-                        <RXGroupOrder></RXGroupOrder>
-                    </Regexp>
-                </ComputerName>
-            </ComputerNameRules>
-        </Rules>
-        <SingleComputers>
-            <SingleComputer>
-                <Name></Name>
-                <Domain></Domain>
-                <UserName></UserName>
-            </SingleComputer>
-        </SingleComputers>
-    </DynamicFolder> 
-</RoyalTSConfiguration>
-'@
-    Try {
-        Write-Verbose -Message "Creating the configuration file $($Path)"
-        # Create the configuration file
-        Set-Content -Path $Path -Value $Configuration -Force | Out-Null
+    }Else{
+        $Message = "The default configuration file [$($DefaultConfigPath)} does not exist"
+        Throw $Message
     }
-    Catch {
-        Write-Error -Message "An unexpected error occured while creating the configuration file $($Path): $($_.Exception.Message)"
-    }
+    
 }
 Function New-RoyalTSDynamicFolder {
     [CmdletBinding()]
-    Param()
+    Param(
+        [String] ${Name} = 'Default'
+    )
     Begin {
         Write-Verbose -Message "Starting the function $($MyInvocation.MyCommand)"
         # Define the location of the configuration file
-        $ConfigurationFile = Join-Path -Path $Env:APPDATA -ChildPath 'SPS-RoyalTS\RoyalTSConfiguration.xml'
+        $ConfigurationFile = Join-Path -Path $Env:APPDATA -ChildPath "SPS-RoyalTS\RoyalTSConfiguration_$($Name).xml"
         # Check if the configuration file exists
         if (-not (Test-Path -Path $ConfigurationFile)) {
-            Write-Verbose -Message "The configuration file $($ConfigurationFile) does not exist"
+            Write-Verbose -Message "The configuration file [$($ConfigurationFile)] does not exist"
             # Create the configuration file
-            New-DefaultRoyalTSConfiguration -Path $ConfigurationFile | out-null
-            Write-Verbose -Message "The configuration file$($ConfigurationFile) has been created"
+            Try {
+                New-DefaultRoyalTSConfiguration -Path $ConfigurationFile | Out-Null
+                Write-Verbose -Message "The configuration file [$($ConfigurationFile)] has been created"
+            }
+            Catch {
+                $Message = "An unexpected error occured while creating the configuration file [$($ConfigurationFile)}: $($_.Exception.Message)"
+                Write-RTSLog -Message $Message -Type Error
+                Throw $Message
+            }
         }
         # Read the configuration file
         [XML] $Configuration = Get-Content -Path $ConfigurationFile -Raw
         # Check if the configuration file has the right structure
         if (-not $Configuration.RoyalTSConfiguration) {
-            Throw "The configuration file $($ConfigurationFile) does not have the right structure"
+            $Message = "The configuration file [$($ConfigurationFile)] does not have the right structure"
+            Write-RTSLog -Message $Message -Type Error
+            Throw $Message
         }
         # Check if the configuration file call a remote file if yes load it instead
         if ($Configuration.RoyalTSConfiguration.RemoteFile) {
-            Write-Verbose -Message "The configuration file $($ConfigurationFile) call a remote file ($($Configuration.RoyalTSConfiguration.RemoteFile))"
+            Write-Verbose -Message "The configuration file [$($ConfigurationFile)] call a remote file [($($Configuration.RoyalTSConfiguration.RemoteFile))]"
             # Get the remote file
             $RemoteFile = $Configuration.RoyalTSConfiguration.RemoteFile
             # Check if the remote file exists
             if (-not (Test-Path -Path $RemoteFile)) {
-                Throw "The remote file [$($RemoteFile)] does not exist"
+                $Message = "The remote file [$($RemoteFile)] declared in [$($ConfigurationFile) - (\RoyalTSConfiguration\RemoteFile)] does not exist"
+                Write-RTSLog -Message $Message -Type Error
+                Throw $Message
             }
             # Read the remote file
             [XML] $Configuration = Get-Content -Path $RemoteFile -Raw
+            $DynamicFolderConfig = $Configuration.RoyalTSConfiguration.DynamicFolder
             # Check if the remote file has the right structure
             if (-not $RemoteConfiguration.RoyalTSConfiguration) {
-                Throw "The remote file [$($RemoteFile)] does not have the right structure"
+                $Message = "The remote file [$($RemoteFile)] does not have the right structure (Missing \RoyalTSConfiguration)"
+                Write-RTSLog -Message $Message -Type Error
+                Throw $Message
             }
+        }Else{
+            $DynamicFolderConfig = $Configuration.RoyalTSConfiguration.DynamicFolder
         }
     }
     Process {
         Write-Verbose -Message "Processing the function $($MyInvocation.MyCommand)"
         # Check if the configuration file has dynamic folders
-        if ($Configuration.RoyalTSConfiguration.DynamicFolder) {
+        if ($DynamicFolderConfig) {
             # Get the dynamic folders rule
         }Else{
-            Write-Warning -Message "The configuration file $($ConfigurationFile) does not have any dynamic folder the function $($MyInvocation.MyCommand) will not do anything"
+            $Message = "The configuration file $($ConfigurationFile) does not have any dynamic folder entry the function $($MyInvocation.MyCommand) will not do anything"
+            Write-RTSLog -Message $Message -Type Warning
+            Write-Warning $Message
         }
     }
     End {
